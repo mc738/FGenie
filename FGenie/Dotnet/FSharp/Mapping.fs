c7250d989583ae63449bdd4afc66925f274c4932
max <me@mclifford.dev>
2024-08-10 17:15:58 +0100

    Work on Mapping.fs

--- FGenie/Dotnet/FSharp/Mapping.fs
+++ FGenie/Dotnet/FSharp/Mapping.fs
@@ -1,10 +1,27 @@
 namespace FGenie.Dotnet.FSharp
 
 open System
+open FGenie.Dotnet.Common
+open FGenie.Dotnet.Common.TypeHelper
 open Microsoft.FSharp.Reflection
 
 module Mapping =
-
+    
+    
+    [<RequireQualifiedAccess>]
+    type CategorizedType =
+        | Base of BaseType
+        | Record
+        | Union
+        | Option of CategorizedType
+        
+        static member TryCreate() =
+            if FSharpType.IsRecord t then
+                CategorizedType.Record
+            elif FSharpType.IsUnion t then
+                CategorizedType.Union
+            elif
+                BaseType. 
     let getUnion (t: Type) =
 
         Ok()
@@ -13,6 +30,9 @@ module Mapping =
         t.GetProperties()
         |> Array.iteri (fun i pi ->
 
+            // Check if the property type is either a base type of record/union.
+            
+            
             pi.pro
 
 
@@ -20,6 +40,7 @@ module Mapping =
 
         Ok()
 
+    
     let get<'T> () =
 
         let t = typeof<'T>
