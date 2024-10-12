b68b16bba09dcaf2883a7a30bc1e7f3c171c98f9
max <me@mclifford.dev>
2024-10-12 16:54:04 +0100

    Work on OpenApi support

---
+++ FGenie.App/Program.fs
@@ -0,0 +1,20 @@
+﻿
+open System
+open Microsoft.FSharp.Reflection
+
+module POC =
+
+    module Iter1 =
+
+        type Foo = { Id: int; Name: string }
+
+        and Bar = { SubId: int; SubName: string }
+
+        let ``parse fsharp record with nested record`` =
+
+            ()
+
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
---
+++ FGenie.App/test.ts
@@ -0,0 +1,5 @@
+class Test {
+    public id: number;
+    public name: string;
+    public b: boolean;
+}
